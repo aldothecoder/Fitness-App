b14c59a3e45423c83792563721660b341ab3d57b
aldothecoder <aldodiazfitness@gmail.com>
2025-06-02 10:52:38 -0500

    added navbar missing code

---
+++ src/Navbar.jsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/Fitness-App/" className="site-title">
+        FitLife Hub
+      </Link>
+
+      <ul>
+        <CustomLink to="/Workouts">Workouts</CustomLink>
+        <CustomLink to="/CalorieCalculator">Calorie Calculator</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
